89f0c439664dc5925126df2392ab34cd0fe8b109
tihanyibenedek <tihbe@digitus.itk.ppke.hu>
2015-11-10 15:57:18 +0100

    Functionality is right!

--- app.js
+++ app.js
@@ -2,30 +2,46 @@ var CalculatorApp = angular.module('CalculatorApp', []);
 
 CalculatorApp.controller('FunctionController', function ($scope) {
 
-	var arg = "";
-	var value = "";
+	var arg1 = "";
+	var arg2 = "";
 	$scope.outputString = "";
 	var operator = "";
 
 	addition = function (add1, add2) {
-		$scope.output = add1*1 + add2*1;
+		//console.log( "Osszeadas " );
+		add1 = add1*1;
+		add2 = add2*1;
+		$scope.output = add1 + add2;
 
 		return $scope.output;
 	};
 
 	substraction = function (sub1, sub2) {
-		$scope.output = sub1*1 - sub2*1;
+		//console.log( "Kivonas " );
+		sub1 = sub1*1;
+		sub2 = sub2*1;
+		$scope.output = sub1 - sub2;
 
 		return $scope.output;
 	};
 
 	multiplication = function (mult1, mult2) {
-		$scope.output = mult1*1 * mult2*1;
+		//console.log( "Szorzas " );
+		//console.log(mult1 + " " + mult2);
+		mult1 = mult1*1;
+		mult2 = mult2*1;
+		//console.log(mult1 + " " + mult2);
+
+		$scope.output = mult1 * mult2;
 
 		return $scope.output;
 	};
 
 	division = function (div1, div2) {
+		//console.log( "Osztas " );
+		div1 = div1*1;
+		div2 = div2*1;
+
 		$scope.output = div1*1 / div2*1;
 
 		return $scope.output;
@@ -37,76 +53,87 @@ CalculatorApp.controller('FunctionController', function ($scope) {
 		$scope.outputString += number;
 	};
 
-	$scope.click = function (buttonClick) {
+	$scope.click = function (selectedNumber) {
 
-		value += buttonClick;
+		if(arg1.length == 0 && arg2.length == 0)
+		{
+			$scope.outputString = "";
+		}
 
-		$scope.outputWrite(buttonClick);
+		//console.log( "eleje: " + "arg1: " + arg1 + " operator: " + operator + " arg2: " + arg2 );
+
+		arg2 += selectedNumber;
+
+		$scope.outputWrite(selectedNumber);
+
+		//console.log( "vege: " + "arg1: " + arg1 + " operator: " + operator + " arg2: " + arg2 );
 	};
 
 	$scope.func = function (functionClick) {
+		
+		if(functionClick == "=")
+		{
+			calculate(functionClick);
+		}
+		else
+		{
+			calculate(operator);
+		}
 
-		switch( functionClick ) {
-		    case '+':
-		    	calculate('+');
-		        $scope.outputString += " + ";
-		        arg = value;
-		    	value = "";
-		        break;
-		    case '-':
-		    	calculate('-');
-		        $scope.outputString += " - ";
-		        arg = value;
-		    	value = "";
-		        break;
-		    case '*':
-		        calculate('*');
-		        $scope.outputString += " * ";
-		        arg = value;
-		    	value = "";
-		        break;
-		    case '/':
-		        calculate('/');
-		        $scope.outputString += " / ";
-		        arg = value;
-		    	value = "";
-		        break;
-		};
+		if(arg1.length == 0)
+		{
+			arg1 = arg2;
+			arg2 = "";
+		}
+		operator = functionClick;
+		if(operator != '=')
+		{
+			$scope.outputString += " " + operator + " ";
+		}
 	};
 
 	$scope.clear = function() {
-		value = "";
+		arg1 = "";
+		arg2 = "";
+		operator = "";
 		$scope.outputString = "";
 	};
 
 	calculate = function(oper) {
-		
-		if(arg)
-		{
-			switch( oper ) {
-			    case '+':
-			    	$scope.outputString = addition(value, arg);
-			        arg = $scope.outputString;
-			        value = "";
-			        break;
-			    case '-':
-			        $scope.outputString = substraction(value, arg);
-			        arg = $scope.outputString;
-			        value = "";
-			        break;
-			    case '*':
-			        $scope.outputString = multiplication(value, arg);
-			        arg = $scope.outputString;
-			        value = "";
-			        break;
-			    case '/':
-			        $scope.outputString = division(value, arg);
-			        arg = $scope.outputString;
-			        value = "";
-			        break;
- 
-			};
-		}
+		//console.log( "arg1 " + arg1 + " arg2 " + arg2 );
 
+		if(arg1.length != 0 && arg2.length != 0)
+		{
+			//console.log( arg1 + " " + operator + " " + arg2 );
+			switch( oper ) {
+				case '+':
+					arg2 = addition(arg1, arg2);
+		 	        $scope.outputString = arg2;
+		 	        arg1 = "";
+					break;
+				case '-':
+					arg2 = substraction(arg1, arg2);
+		 	        $scope.outputString = arg2;
+					arg1 = "";
+					break;
+				case '*':
+					arg2 = multiplication(arg1, arg2);
+		 	        $scope.outputString = arg2;
+					arg1 = "";
+					break;
+				case '/':
+					arg2 = division(arg1, arg2);
+		 	        $scope.outputString = arg2;
+					arg1 = "";
+					break;
+				case '=':
+					calculate(operator);
+					arg1 = "";
+					arg2 = "";
+					operator = "";
+					//console.log( "end of equal" );
+					break;
+			};
+		}	
 	};
 });
